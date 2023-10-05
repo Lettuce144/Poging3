0f07c26e86e0a575ec97c5c64f17d578de1a947b
spame978 <zeefjens@gmail.com>
2023-10-05 21:10:00 +0200

    Hi

---
+++ src/main/java/daje/edescent/E_Descent_3.java
@@ -0,0 +1,17 @@
+package daje.edescent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class E_Descent_3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
